39d4f4408542f2fbe417fdf8fc524c537db4c102
CameLion <31916757+CameLion@users.noreply.github.com>
2019-07-21 11:52:56 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/oscarsun95/computeForFinance""
    This reverts commit c22709dad84a54634b7a859d10136f615ef48d4d.

--- SingleStock_SingleStockFuturesArbitrageStrategy.py
+++ SingleStock_SingleStockFuturesArbitrageStrategy.py
@@ -8,6 +8,7 @@ Created on July 17 2019
 
 import os
 import time
+from typing import Dict
 from common.OrderBookSnapshot_FiveLevels import OrderBookSnapshot_FiveLevels
 from common.Strategy import Strategy
 from common.SingleStockOrder import SingleStockOrder
@@ -30,22 +31,28 @@ class SingleStock_SingleStockFuturesArbitrageStrategy(Strategy):
     def getStratDay(self):
         return self.day
 
-    def on_marketData(self, marketData):
+    def on_marketData(self, marketData: Dict[str, OrderBookSnapshot_FiveLevels]):
         # handle new market data
-        singleStockOrder = SingleStockOrder('testTicker', '2019-07-05', time.asctime(time.localtime(time.time())))
+        singleStockOrder = SingleStockOrder('stock', '2019-07-05', time.asctime(time.localtime(time.time())))
         singleStockOrder.submissionTime = time.asctime(time.localtime(time.time()))
         singleStockOrder.currStatus = "New"  # "New", "Filled", "PartiallyFilled", "Cancelled"
         singleStockOrder.direction = 1
-        singleStockOrder.price = marketData.outputAsDataFrame()['askPrice1']
-        singleStockOrder.size = (self.cash > 0) * self.cash * 0.2 // singleStockOrder.price
+        singleStockOrder.price = marketData['stock'].askPrice1
+        singleStockOrder.size = self.cash * 0.2 // singleStockOrder.price
         print('*******strat size: ', singleStockOrder.size)
         singleStockOrder.type = "MO"  # "MLO", "LO", "MO", "TWAP"
+        # TODO:: just a sample, need to add future order details
+        singleFutureOrder = singleStockOrder.copyOrder()
+        singleFutureOrder.ticker = 'future'
+        singleFutureOrder.price = marketData['future'].bidPrice1
+        singleFutureOrder.direction = singleStockOrder.direction * -1
+        return [singleStockOrder, singleFutureOrder]
 
-        return singleStockOrder
-
-    def on_execution(self, execution):
+    def on_execution(self, executions: Dict[str, SingleStockExecution]):
         # handle executions
         print('[%d] Strategy.handle_execution' % (os.getpid()))
+        execution = executions['stock']
+        # TODO::need to handle future execution
         print('execution:', execution.outputAsArray())
 
         self.currStatusTime = execution.timeStamp
